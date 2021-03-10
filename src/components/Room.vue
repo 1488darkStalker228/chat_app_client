b032d1bc008356f5ae8797c73bceb333e857860d
redhat <redhat@localhost.localdomain>
2021-03-10 18:08:35 +0300

    commit

--- src/components/Room.vue
+++ src/components/Room.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="room pa-6">
-    <div class="room__messages pa-6" >
+    <div class="room__messages pa-6" ref="messagesArea">
       <ul class="room__list">
         <li v-for="(message, index) of messages" :key="index">
           {{message.userName}}: {{message.message}}
@@ -21,6 +21,13 @@
         label="Message"
         type="text"
     />
+
+    <div class="room__input-wrap">
+      <input type="file" ref="fileInput">
+      <button class="room__btn" @click="encodeImageFileURL">
+        Отправить
+      </button>
+    </div>
   </div>
 </template>
 
@@ -32,12 +39,20 @@
     name: "Room",
 
     created() {
-      socket.on('CHAT_MESSAGE', (data) => this.messages.push(data));
+      socket.on('CHAT_MESSAGE', (data) => {
+        console.log(data);
+        this.messages.push(data)
+      });
+
+      socket.on('CHAT_IMAGE', (data) => {
+        console.log(data);
+        const image = document.createElement('img');
+        image.src = data.img;
+        document.body.append(image);
+      });
     },
 
-    data() {
-      return {messages: [], message: ''}
-    },
+    data() {return {messages: [], message: ''}},
 
     methods: {
       sendMessage() {
@@ -45,12 +60,26 @@
           socket.emit('CHAT_MESSAGE', {message: this.message, userName: this.USER_NAME});
           this.message = '';
         }
+      },
+
+      encodeImageFileURL() {
+        fileSelect = this.$refs.fileInput.files;
+
+        if (fileSelect.length > 0) {
+          var fileSelect = fileSelect[0];
+          var fileReader = new FileReader();
+
+          fileReader.onload = function(FileLoadEvent) {
+            var srcData = FileLoadEvent.target.result;
+            socket.emit('CHAT_IMAGE', {img: srcData});
+          }
+          fileReader.readAsDataURL(fileSelect);
+        }
       }
     },
 
-    computed: {
-      ...mapGetters(['USERS', 'USER_NAME'])
-    }
+    computed: {...mapGetters(['USERS', 'USER_NAME'])}
+
   }
 </script>
 
@@ -66,6 +95,7 @@
       border: 1px solid;
       border-radius: 7px;
       overflow: auto;
+      position: relative;
     }
 
     &__list {
@@ -81,7 +111,32 @@
     }
 
     &__message-input {
+      margin-top: 8px;
       flex-grow: 0;
+    }
+
+    &__attach-file {
+      position: absolute;
+      right: 65px;
+      bottom: 35px;
+      margin: 0;
+    }
+
+    &__input-wrap {
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+      padding: 8px 0;
+    }
+
+    &__btn {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 35px;
+      background: antiquewhite;
+      border-radius: 22px;
+      padding: 8px;
     }
   }
 </style>
